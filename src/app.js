51cafa8807acf5aa50e54e9acdccb64a2c8198c6
Sapereza <s.perezanderson@gmail.com>
2022-08-06 21:54:50 -0400

    Added the function to toggle the comments section

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
-//Get my data or elements
+//Get my data or elements: this is to toggle the search section
 const searchBox = document.getElementById('search-box');
 const resultBox = document.getElementById('results');
 
 searchBox.addEventListener('click', function(){
     results.classList.toggle('hidden');
-    
+
 })
+
+//Toggle the Comments section onclick
+
+function showcomment(){
+    var x =
+document.getElementById('comment');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+   
