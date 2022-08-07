9c4931b308b8f3cfa742ccf6bd0ecff7d9753cfe
Sapereza <s.perezanderson@gmail.com>
2022-08-06 21:18:44 -0400

    Created the js script for the search box so that the hidden results area toggles when you click withing the box

---
+++ src/app.js
@@ -0,0 +1,8 @@
+//Get my data or elements
+const searchBox = document.getElementById('search-box');
+const resultBox = document.getElementById('results');
+
+searchBox.addEventListener('click', function(){
+    results.classList.toggle('hidden');
+    
+})
